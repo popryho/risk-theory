04b32966e2a7ed80992807d3f8322c32abca2982
Leonid Popryho <18726515+popryho@users.noreply.github.com>
2021-03-18 08:48:01 +0200

    output improvements

--- lab3/task1.py
+++ lab3/task1.py
@@ -8,21 +8,23 @@ from const import gamma
 def empty_cell(alpha):
     sample_1 = np.sort(np.random.exponential(scale=1, size=n))
     sample_2 = np.random.exponential(scale=1 / alpha, size=m)
-    k = 0
+    k_value = 0
     for i in range(1, sample_1.shape[0]):
         mask = np.logical_and(sample_2 > sample_1[i - 1], sample_1[i] > sample_2)
-        k += 1 if sample_2[mask].shape[0] == 0 else 0
+        k_value += 1 if sample_2[mask].shape[0] == 0 else 0
 
     ro = m / n
     criterion = n / (1 + ro) + np.sqrt(n) * ro * norm.ppf(q=1 - gamma) / np.power(1 + ro, 3 / 2)
 
-    if k > criterion:
-        return f'NOT Accepted, k = {k}, criterion = {criterion}'
+    if k_value > criterion:
+        return f'k = {k_value}, criterion = {criterion:0.4f}. \n' \
+               f'The statistical data do CONFLICT with the H0 hypothesis.'
     else:
-        return f'Accepted, k = {k}, criterion = {criterion}'
+        return f'k = {k_value}, criterion = {criterion:0.4f}. \n' \
+               f'The statistical data do NOT CONFLICT with the H0 hypothesis.'
 
 
-def chi_square(alpha, r=3):
+def chi_square(alpha):
 
     sample_1 = np.random.exponential(scale=1, size=n)
     sample_2 = np.random.exponential(scale=1, size=m)
@@ -48,17 +50,30 @@ def chi_square(alpha, r=3):
 
     criterion = chdtri(r - 1, gamma)
     if delta > criterion:
-        return f'Not accepted , delta = {delta}, criterion = {criterion}'
+        return f'Delta = {delta:0.4f}, criterion = {criterion:0.4f}. \n' \
+               f'The statistical data do CONFLICT with the H0 hypothesis.'
     else:
-        return f'Accepted , delta = {delta}, criterion = {criterion}'
+        return f'Delta = {delta:0.4f}, criterion = {criterion:0.4f}. \n' \
+               f'The statistical data do NOT CONFLICT with the H0 hypothesis.'
 
 
 if __name__ == '__main__':
-    a = 1.1
-    for m in map(int, (1e3, 1e4, 1e5)):
-        n = int(m / 2)
-        print(f'{"-" * 50}\nn = {n}, m = {m}, {empty_cell(alpha=a)}')
 
-    for n in map(int, (2e2, 2e3, 2e4)):
-        m, k = 3 * n, 2 * n
-        print(f'{"-" * 50}\nn = {n}, m = {m}, k = {k}, {chi_square(alpha=a)}')
+    a = 1.1
+    r = 4
+    with open('output/output_task1.txt', 'w') as txt:
+        """
+        EXAMPLE. EMPTY-BOXES test.
+        """
+        txt.write(f'TASK A. \n\ngamma = {gamma} => z_gamma = {norm.ppf(q=1 - gamma):0.4f}\n')
+        for m in map(int, (1e3, 1e4, 1e5)):
+            n = int(m / 2)
+            txt.write(f'{"-" * 50}\nn={n}, m={m}, {empty_cell(alpha=a)}\n')
+
+        """
+        EXAMPLE. CHI-SQUARE test.
+        """
+        txt.write(f'\nTASK B.\n\nr = {r}\n')
+        for n in map(int, (2e2, 2e3, 2e4)):
+            m, k = 3 * n, 2 * n
+            txt.write(f'{"-" * 50}\nn={n}, m={m}, k={k}, {chi_square(alpha=a)}\n')
