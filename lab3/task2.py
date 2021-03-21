ea16aea53dc6eb485d61f3999272e063958a0fa4
Leonid Popryho <18726515+popryho@users.noreply.github.com>
2021-03-21 19:54:15 +0200

    output improvements

--- lab3/task2.py
+++ lab3/task2.py
@@ -1,78 +1,72 @@
 import numpy as np
+import pandas as pd
 from scipy.special import chdtri
 from scipy.stats import spearmanr
 from scipy.stats import kendalltau
 from const import gamma
 
 
-def chi_square(X, Y, n, m):
-    n_total = n + m
-    v = np.zeros((n, m))
-    split_list1 = np.linspace(0, 10, n)
-    split_list2 = np.linspace(0, 10, m)
-    for i_split in range(1, n):
-        for j_split in range(1, m):
-            a1 = X[X > split_list1[i_split - 1]]
-            b1 = X[X < split_list1[i_split]]
-            a2 = Y[Y > split_list2[j_split - 1]]
-            b2 = Y[Y < split_list2[j_split]]
-            v[i_split - 1, j_split - 1] = np.intersect1d(a1, b1).shape[0] + np.intersect1d(a2, b2).shape[0]
+def chi_square():
+
+    sample_1 = np.random.uniform(0, 1, size=n)
+    sample_2 = sample_1 + np.random.uniform(-1, 1, size=n)
+
+    df = pd.DataFrame(data=[sample_1, sample_2]).T
+
+    bins_x = np.linspace(sample_1.min(initial=0).round(), sample_1.max(initial=1).round(), num=r)
+    bins_y = np.linspace(sample_2.min(initial=-1).round(), sample_2.max(initial=2).round(), num=k)
+
+    df.iloc[:, 0] = bins_x[np.digitize(df.iloc[:, 0], bins=bins_x)]
+    df.iloc[:, 1] = bins_y[np.digitize(df.iloc[:, 1], bins=bins_y)]
+
+    v = pd.crosstab(df.iloc[:, 0], df.iloc[:, 1]).to_numpy()
+
+    n_total = r + k
     delta = 0
-    for i in range(1, n - 1):
-        for j in range(1, m - 1):
+    for i in range(v.shape[0]):
+        for j in range(v.shape[1]):
             delta += ((v[i, j] - (v[i, :].sum() * v[:, j].sum()) / n_total) ** 2) / (v[i, :].sum() * v[:, j].sum())
     delta *= n_total
-    krit = chdtri((n - 1) * (m - 1), gamma)
+    criterion = chdtri((r - 1) * (k - 1), gamma)
 
-    if delta > krit:
-        return f"Not accepted , delta = {delta}, krit = {krit}"
+    if delta > criterion:
+        return f'delta = {delta:0.4f}, criterion = {criterion:0.4f} \n' \
+               f'The statistical data do CONFLICT with the H0 hypothesis.'
     else:
-        return f"Accepted , delta = {delta}, krit = {krit}"
+        return f'delta = {delta:0.4f}, criterion = {criterion:0.4f} \n' \
+               f'The statistical data do NOT CONFLICT with the H0 hypothesis.'
 
 
-def spearm_test(data1, data2):
-    correl, critical_value = spearmanr(data1, data2)
-    # significance level
-    alpha = 0.05
-    if critical_value > alpha:
-        return 'Fail to reject H0 critical_value=%.3f' % critical_value
+def spearman_test():
+    sample_1 = np.random.uniform(0, 1, size=n)
+    sample_2 = sample_1 + np.random.uniform(-1, 1, size=n)
+    corr, critical_value = spearmanr(sample_1, sample_2)
+
+    if critical_value > gamma:
+        return f'critical_value={critical_value:.3f} \n' \
+               f'The statistical data do NOT CONFLICT with the H0 hypothesis.'
+
     else:
-        return 'Reject H0 critical_value=%.3f' % critical_value
+        return f'critical_value={critical_value:.3f} \n' \
+               f'The statistical data do CONFLICT with the H0 hypothesis.'
 
 
-def kendall_test(data1, data2):
-    correl, critical_value = kendalltau(data1, data2)
-    # significance level
-    alpha = 0.05
-    if critical_value > alpha:
-        return 'Fail to reject H0 critical_value=%.3f' % critical_value
+def kendall_test():
+    sample_1 = np.random.uniform(0, 1, size=n)
+    sample_2 = sample_1 + np.random.uniform(-1, 1, size=n)
+    corr, critical_value = kendalltau(sample_1, sample_2)
+
+    if critical_value > gamma:
+        return f'critical_value={critical_value:.3f} \n' \
+               f'The statistical data do NOT CONFLICT with the H0 hypothesis.'
+
     else:
-        return 'Reject H0 critical_value=%.3f' % critical_value
-
-
-def task_a():
-    r, k = 15, 18
-    for n in map(int, (5e2, 5e3, 5e4)):
-        X = np.random.uniform(0, 1, size=n)
-        Y = np.random.uniform(-1, 1, size=n)
-        print(f'n = {n}, {chi_square(X, X + Y, r, k)}')
-
-
-def task_b():
-    for n in map(int, (5e2, 5e3, 5e4)):
-        X = np.random.uniform(0, 1, size=n)
-        Y = np.random.uniform(-1, 1, size=n)
-        print(f'n = {n}, {spearm_test(X, X + Y)}')
-
-
-def task_c():
-    for n in map(int, (5e2, 5e3, 5e4)):
-        X = np.random.uniform(0, 1, size=n)
-        Y = np.random.uniform(-1, 1, size=n)
-        print(f"n = {n}, {kendall_test(X, X + Y)}")
+        return f'critical_value={critical_value:.3f} \n' \
+               f'The statistical data do CONFLICT with the H0 hypothesis.'
 
 
 if __name__ == '__main__':
-    task_a()
-    task_b()
-    task_c()
+    r, k = 100, 100
+    for method in (chi_square, spearman_test, kendall_test):
+        for n in map(int, (5e2, 5e3, 5e4)):
+            print(f'n = {n}, {method()}')
