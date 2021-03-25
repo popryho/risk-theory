7d7f0913d800ab6abd5eb247fb32ec952df94d78
popryho <18726515+popryho@users.noreply.github.com>
2021-03-25 11:13:23 +0200

    using rankdata from scipy.stats to increase speed in spearman and kendall methods. output added for the second task

--- lab3/task2.py
+++ lab3/task2.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
 from scipy.special import chdtri
-from scipy.stats import spearmanr
-from scipy.stats import kendalltau
+from scipy.stats import norm, rankdata
+
 from const import gamma
-from scipy.stats import norm
 
 
 def chi_square():
@@ -30,67 +29,56 @@ def chi_square():
     criterion = chdtri((r - 1) * (k - 1), gamma)
 
     if delta > criterion:
-        return f'delta = {delta:0.4f}, criterion = {criterion:0.4f} \n' \
-               f'The statistical data do CONFLICT with the H0 hypothesis.'
+        return f'r = {r}, k = {k}, delta = {delta:0.4f}, criterion = {criterion:0.4f} \n' \
+               f'The statistical data do CONFLICT with the H0 hypothesis. \n'
     else:
         return f'delta = {delta:0.4f}, criterion = {criterion:0.4f} \n' \
-               f'The statistical data do NOT CONFLICT with the H0 hypothesis.'
+               f'The statistical data do NOT CONFLICT with the H0 hypothesis. \n'
 
 
 def spearman():
     sample_1 = np.random.uniform(0, 1, size=n)
     sample_2 = sample_1 + np.random.uniform(-1, 1, size=n)
 
-    sample_1_sorted = np.sort(sample_1)
-    sample_2_sorted = np.sort(sample_2)
-
-    r_value = np.array([np.where(sample_1_sorted == x)[0][0] + 1 for x in sample_1])
-    s_value = np.array([np.where(sample_2_sorted == y)[0][0] + 1 for y in sample_2])
-
-    p = np.abs(1 - 6 / (n * (n ** 2 - 1)) * np.sum((r_value - s_value) ** 2))
+    p = np.abs(1 - 6 / (n * (n ** 2 - 1)) * np.sum((rankdata(sample_1) - rankdata(sample_2)) ** 2))
     criterion = norm.ppf(1 - 0.05 / 2) / np.sqrt(n)
 
     if p > criterion:
         return f'|p| = {p:.3f}, criterion = {criterion:.3f} \n' \
-               f'The statistical data do CONFLICT with the H0 hypothesis.'
+               f'The statistical data do CONFLICT with the H0 hypothesis. \n'
 
     else:
         return f'|p| = {p:.3f}, criterion = {criterion:.3f} \n' \
-               f'The statistical data do NOT CONFLICT with the H0 hypothesis.'
+               f'The statistical data do NOT CONFLICT with the H0 hypothesis. \n'
 
 
 def kendall():
     sample_1 = np.random.uniform(0, 1, size=n)
     sample_2 = sample_1 + np.random.uniform(-1, 1, size=n)
 
-    sample_1_sorted = np.sort(sample_1)
-    sample_2_sorted = np.sort(sample_2)
-
-    r_value = np.array([np.where(sample_1_sorted == x)[0][0] + 1 for x in sample_1])
-    s_value = np.array([np.where(sample_2_sorted == y)[0][0] + 1 for y in sample_2])
-
-    print(r_value, np.argsort())
-    pairs = sorted(list(zip(r_value, s_value)), key=lambda x: x[0])
-    V = np.array(list(zip(*pairs))[1])
-    N = 0
-    for i in range(n):
-        subV = V[i + 1:]
-        N += len(subV[V[i - 1] < subV])
+    ranks = np.concatenate([rankdata(sample_1).reshape(-1, 1),
+                            rankdata(sample_2).reshape(-1, 1)], axis=1)
+    v = ranks[ranks[:, 0].argsort()][:, 1]
+    N = int(np.sum([len(v[i + 1:][v[i - 1] < v[i + 1:]]) for i in range(n)]))
 
     tau = np.abs((4 * N) / (n * (n - 1)) - 1)
     criterion = 2 * norm.ppf(1 - 0.05 / 2) / (3 * np.sqrt(n))
 
     if tau > criterion:
         return f'|tau| = {tau:.3f}, criterion = {criterion:.3f} \n' \
-               f'The statistical data do CONFLICT with the H0 hypothesis.'
+               f'The statistical data do CONFLICT with the H0 hypothesis. \n'
 
     else:
         return f'|tau| = {tau:.3f}, criterion = {criterion:.3f} \n' \
-               f'The statistical data do NOT CONFLICT with the H0 hypothesis.'
+               f'The statistical data do NOT CONFLICT with the H0 hypothesis. \n'
 
 
 if __name__ == '__main__':
+
     r, k = 10, 20
-    for method in (chi_square, spearman, kendall):
-        for n in map(int, (5e2, 5e3, 5e4)):
-            print(f'n = {n}, {method()}')
+    with open('output/output_task2.txt', 'w') as txt:
+        txt.write("TASK 2")
+        for idx, method in enumerate((chi_square, spearman, kendall)):
+            txt.write(f'\n\nMETHOD {idx+1}: {method.__name__}\n')
+            for n in map(int, (5e2, 5e3, 5e4)):
+                txt.write(f'{"-" * 50}\nn = {n}, {method()}')
