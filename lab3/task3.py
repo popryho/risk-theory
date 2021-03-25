ee5f0759fb09d5de712453e7cd344bd2087d81e5
Leonid Popryho <18726515+popryho@users.noreply.github.com>
2021-03-25 11:23:17 +0200

    task 3 code improvements

--- lab3/task3.py
+++ lab3/task3.py
@@ -1,3 +1,42 @@
+import scipy.stats
+import numpy as np
+
+
+def number_inversion(X):
+    count = 0
+    for i in range(1, len(X) - 1):
+        for j in range(i):
+            if X[j] > X[i]:
+                count += 1
+    return count
+
+
+def calculate_confidence_interval(n):
+    mean = (n * (n - 1)) / 4
+    std = np.sqrt((n * (n - 1) * (2 * n + 5)) / 72)
+    h = std * scipy.stats.t.ppf((1 + .95) / 2., n - 1)
+    return mean - h, mean + h
+
+
+if __name__ == '__main__':
+
+    for n in map(int, (5e2, 5e3, 5e4)):
+
+        X = np.random.uniform(-1, 1, size=n)
+
+        n_inversions = number_inversion(X)
+        left, right = calculate_confidence_interval(n)
+
+        print(f'Allowed interval for Normal distribution is [{left:.1f}, {right:.1f}]')
+        if left < n_inversions < right:
+            print(f'Number of inversions = {n_inversions}. '
+                  f'The statistical data do NOT CONFLICT with the H0 hypothesis.')
+        else:
+            print(f'Number of inversions = {n_inversions} '
+                  f'The statistical data do CONFLICT with the H0 hypothesis.')
+
+
+
 def form_X(n):
     ksi = np.random.uniform(low=-1.0, high=1.0, size=n)
     X = np.zeros(n)
@@ -22,7 +61,7 @@ def independence(X):
     n = len(X)
     eta = calc_eta(X)
     S = np.sum(eta)
-    z = st.norm.ppf(1 - 0.05 / 2)
+    z = norm.ppf(1 - 0.05 / 2)
     criteria = abs((6 / (n ** (3 / 2))) * (S - ((n * (n - 1)) / 4)))
     print(f'n = {n}')
     print(f'Приблизні значення вибірки Х: {X[0:4]}')
